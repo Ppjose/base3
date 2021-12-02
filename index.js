752a88fdea12f529cffe9afc8a0d5590f9855af6
ppjose <josepalomare@gmail.com>
2021-12-02 14:23:23 +0100

    tercerocommit

---
+++ index.js
@@ -0,0 +1,10 @@
+function calcula_resta(){
+	
+    var numero1 = document.getElementById("numero1").value;
+	var numero2 = document.getElementById("numero2").value;
+
+	var resultado = (numero1 - numero2);
+
+	alert ("El resultado es : " + resultado);
+
+}
